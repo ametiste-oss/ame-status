ca637d72c9e2b642480629a4176d31c555141fc9
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-18 12:52:54 +0300

    + initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='amestatus',
+    version='0.1.0',
+    description="Status page integration toolkit, lightweight and flexy",
+    long_description=open('README.md').read(),
+    author="Ametiste-OSS",
+    author_email='masted@gmail.com',
+    url='https://github.com/ametiste-oss/ame-status',
+    license='MIT',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
